013b24d52eaacf8e072f7c76493359e9cb357ef2
Syed Saad Ali <foxrot.tesla@gmail.com>
2026-07-17 04:30:49 +0530

    Fix: sp error

--- app/src/main/java/com/someoddguy/snapshare/ui/connectionvalidationscreen/ConnectionValidationScreen.kt
+++ app/src/main/java/com/someoddguy/snapshare/ui/connectionvalidationscreen/ConnectionValidationScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.someoddguy.snapshare.R
